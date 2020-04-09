0ea683889f4a895855876d0345dfd98babbad6ba
Liviubaws <liviu.pnz97@gmail.com>
2020-04-09 14:18:18 +0300

    subiect b.1

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
     private Button button;
+    private Button buttonAdd;
+    private EditText upEditText;
+    private EditText downEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -16,16 +20,35 @@ public class Colocviu1_245MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_practical_test01_245_main);
 
         button = (Button) findViewById(R.id.down_button);
+        buttonAdd = (Button) findViewById(R.id.up_button);
+        upEditText = (EditText) findViewById(R.id.up_edit_text);
+        downEditText = (EditText) findViewById(R.id.down_edit_text);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String setText = String.valueOf(upEditText.getText().toString());
+                String theText = String.valueOf(downEditText.getText().toString());
+
+                switch(v.getId()) {
+                    case R.id.up_button:
+                        if(setText.matches("-?(0|[1-9]\\d*)")) {
+                            theText = theText.concat("+");
+                            theText = theText.concat(setText);
+                            downEditText.setText(theText);
+                        }
+                        break;
+                }
+            }
+        });
     }
 
     public void openActivity2(){
-        Intent intent = new Intent(this, Activity2.class);
+        Intent intent = new Intent(this, Colocviu1_245SecondaryActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.Colocviu1_245;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected int onCreate(Bundle savedInstanceState) {
+        int sum = 0;
+        super.onCreate(savedInstanceState);
+        return sum;
+    }
+}
