bdbfe85ce8a861086a3cc2af0eb82c8bb992b62a
Liviubaws <liviu.pnz97@gmail.com>
2020-04-09 13:55:29 +0300

    subiect 1 tot

---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Activity2.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.Colocviu1_245;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -2,13 +2,30 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_245_main);
+
+        button = (Button) findViewById(R.id.down_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
