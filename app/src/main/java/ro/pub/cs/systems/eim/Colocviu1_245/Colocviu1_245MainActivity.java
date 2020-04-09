46ba1a0c63785d29b8bf80a5fedb6640a4da283d
Liviubaws <liviu.pnz97@gmail.com>
2020-04-09 14:40:02 +0300

    b2 si c1 cred

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
     private Button button;
     private Button buttonAdd;
     private EditText upEditText;
     private EditText downEditText;
+    private static final int SECOND_ACTIVITY_REQUEST_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -49,6 +51,26 @@ public class Colocviu1_245MainActivity extends AppCompatActivity {
 
     public void openActivity2(){
         Intent intent = new Intent(this, Colocviu1_245SecondaryActivity.class);
-        startActivity(intent);
+        //startActivity(intent); - ASTA ERA LA A
+        intent.putExtra("theSum",downEditText.getText().toString());
+        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check that it is the SecondActivity with an OK result
+        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                // Get String data from Intent
+                String returnString = data.getStringExtra("suma");
+
+                Toast.makeText(Colocviu1_245MainActivity.this, (String)returnString,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -2,14 +2,53 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
+    public static int findSum(String str)
+    {
+        // A temporary string
+        String temp = "";
 
-    @Override
-    protected int onCreate(Bundle savedInstanceState) {
+        // holds sum of all numbers present in the string
+        int sum = 0;
+
+        // read each character in input string
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+
+            // if current character is a digit
+            if (Character.isDigit(ch))
+                temp += ch;
+
+                // if current character is an alphabet
+            else
+            {
+                // increment sum by number found earlier
+                // (if any)
+                sum += Integer.parseInt(temp);
+
+                // reset temporary string to empty
+                temp = "0";
+            }
+        }
+
+        // atoi(temp.c_str()) takes care of trailing
+        // numbers
+        return sum + Integer.parseInt(temp);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
         int sum = 0;
         super.onCreate(savedInstanceState);
-        return sum;
+        Intent i = getIntent();
+        String theSum = i.getStringExtra("theSum");
+        sum = findSum(theSum);
+        Intent intent = new Intent();
+        intent.putExtra("suma", Integer.toString(sum));
+        setResult(RESULT_OK, intent);
+        finish();
+        //return sum;
     }
 }
